354df35ee90527ec362867cb2690e5a9d4a65e19
Tiffany Marthin <tiffanymarthin@gmail.com>
2021-05-17 22:32:39 -0700

    Add default config for default api client

--- src/main/java/neu/cs6650/Configuration.java
+++ src/main/java/neu/cs6650/Configuration.java
@@ -1,2 +1,27 @@
-package neu.cs6650;public class Configuration {
+package neu.cs6650;
+
+import neu.cs6650.client.ApiClient;
+
+public class Configuration {
+  private static ApiClient defaultApiClient = new ApiClient();
+
+  /**
+   * Get the default API client, which would be used when creating API
+   * instances without providing an API client.
+   *
+   * @return Default API client
+   */
+  public static ApiClient getDefaultApiClient() {
+    return defaultApiClient;
+  }
+
+  /**
+   * Set the default API client, which would be used when creating API
+   * instances without providing an API client.
+   *
+   * @param apiClient API client
+   */
+  public static void setDefaultApiClient(ApiClient apiClient) {
+    defaultApiClient = apiClient;
+  }
 }
