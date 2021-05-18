a13e0ac348bfd8f3908eec74421a5d2bb222716a
Tiffany Marthin <tiffanymarthin@gmail.com>
2021-05-18 14:59:23 -0700

    Textbody Api now implements the callable

--- src/main/java/neu/cs6650/api/TextbodyApi.java
+++ src/main/java/neu/cs6650/api/TextbodyApi.java
@@ -2,22 +2,28 @@ package neu.cs6650.api;
 
 import com.google.gson.reflect.TypeToken;
 import java.lang.reflect.Type;
+import java.util.concurrent.Callable;
 import neu.cs6650.Configuration;
 import neu.cs6650.client.ApiClient;
 import neu.cs6650.model.ResultVal;
 import neu.cs6650.model.TextLine;
+import neu.cs6650.model.ThreadRecord;
 import okhttp3.Call;
 
-public class TextbodyApi {
+public class TextbodyApi implements Callable<ThreadRecord> {
 
   private ApiClient apiClient;
+  private TextLine body;
+  private String function;
 
 //  public TextbodyApi() {
 //    this(Configuration.getDefaultApiClient());
 //  }
 
-  public TextbodyApi(ApiClient apiClient) {
+  public TextbodyApi(ApiClient apiClient, TextLine body, String function) {
     this.apiClient = apiClient;
+    this.body = body;
+    this.function = function;
   }
 
   public ApiClient getApiClient() {
@@ -28,32 +34,45 @@ public class TextbodyApi {
     this.apiClient = apiClient;
   }
 
-  public Call analyzeNewCall(TextLine body, String function) {
-    Object localVarPostBody = body;
-
-    // create path and map variables
-    final String localVarPath = "/textbody/" + function + "/";
-    final String localVarContentType = "application/json; charset=utf-8";
-
-    return apiClient.buildPostCall(localVarPath, localVarPostBody, localVarContentType);
+  public TextLine getBody() {
+    return body;
   }
 
-  private Call analyzeValidateNewCall(TextLine body, String function) throws ApiException {
+  public String getFunction() {
+    return function;
+  }
+
+  @Override
+  public ThreadRecord call() throws ApiException {
     // verify the required parameter 'body' is set
-    if (body == null) {
+    if (this.body == null) {
       throw new ApiException("Missing the required parameter 'body'");
     }
     // verify the required parameter 'function' is set
-    if (function == null) {
+    if (this.function == null) {
       throw new ApiException("Missing the required parameter 'function'");
     }
-    return analyzeNewCall(body, function);
+
+    int totalSuccessCall = 0, totalFailedCall = 0;
+    Object localVarPostBody = this.body;
+
+    // create path and map variables
+    final String localVarPath = "/textbody/" + this.function + "/";
+    final String localVarContentType = "application/json; charset=utf-8";
+
+    if (apiClient.postRequest(localVarPath, localVarPostBody, localVarContentType)) {
+      totalSuccessCall++;
+    } else {
+      totalFailedCall++;
+    }
+    return new ThreadRecord(totalSuccessCall, totalFailedCall);
   }
 
-  public ResultVal analyzeNewLine(TextLine body, String function) throws ApiException {
-    Call call = analyzeValidateNewCall(body, function);
-    Type localVarReturnType = new TypeToken<ResultVal>(){}.getType();
-//    ApiResponse<ResultVal> resp = apiClient.execute(call, localVarReturnType);
-    return resp.getData();
-  }
+
+//  public ResultVal analyzeNewLine() throws ApiException {
+//    Call call = analyzeValidateNewCall();
+//    Type localVarReturnType = new TypeToken<ResultVal>(){}.getType();
+////    ApiResponse<ResultVal> resp = apiClient.execute(call, localVarReturnType);
+//    return resp.getData();
+//  }
 }
