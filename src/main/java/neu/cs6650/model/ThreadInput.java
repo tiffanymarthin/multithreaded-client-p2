d811a3faf5bbce6014ac1486268cd334ab416514
Tiffany Marthin <tiffanymarthin@gmail.com>
2021-05-17 19:05:10 -0700

    Implemented the ThreadInput class

--- src/main/java/neu/cs6650/model/ThreadInput.java
+++ src/main/java/neu/cs6650/model/ThreadInput.java
@@ -2,4 +2,38 @@ package neu.cs6650.model;
 
 public class ThreadInput {
 
+  private String ipAddress;
+  private String port;
+  private Integer startTime;
+  private Integer endTime;
+  private Integer numOfTask;
+
+  public ThreadInput(String ipAddress, String port, Integer startTime, Integer endTime,
+      Integer numOfTask) {
+    this.ipAddress = ipAddress;
+    this.port = port;
+    this.startTime = startTime;
+    this.endTime = endTime;
+    this.numOfTask = numOfTask;
+  }
+
+  public String getIpAddress() {
+    return ipAddress;
+  }
+
+  public String getPort() {
+    return port;
+  }
+
+  public Integer getStartTime() {
+    return startTime;
+  }
+
+  public Integer getEndTime() {
+    return endTime;
+  }
+
+  public Integer getNumOfTask() {
+    return numOfTask;
+  }
 }
