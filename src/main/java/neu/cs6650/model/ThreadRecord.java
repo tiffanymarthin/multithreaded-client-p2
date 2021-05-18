6b875105dd02c2cb0b113e4fb12bbebe4d1cf623
Tiffany Marthin <tiffanymarthin@gmail.com>
2021-05-17 19:00:00 -0700

    Implement simple thread record class

--- src/main/java/neu/cs6650/model/ThreadRecord.java
+++ src/main/java/neu/cs6650/model/ThreadRecord.java
@@ -1,5 +1,19 @@
 package neu.cs6650.model;
 
 public class ThreadRecord {
+  private int nSuccessRequest;
+  private int nFailedRequest;
 
+  public ThreadRecord(int nSuccessRequest, int nFailedRequest) {
+    this.nSuccessRequest = nSuccessRequest;
+    this.nFailedRequest = nFailedRequest;
+  }
+
+  public int getNSuccessRequest() {
+    return nSuccessRequest;
+  }
+
+  public int getNFailedRequest() {
+    return nFailedRequest;
+  }
 }
