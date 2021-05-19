bd8dc7fd40c45a38d68107e52e635ab04fc778de
Tiffany Marthin <tiffanymarthin@gmail.com>
2021-05-19 14:30:58 -0700

    Add config class to store aws vs local

--- src/main/java/neu/cs6650/Configuration.java
+++ src/main/java/neu/cs6650/Configuration.java
@@ -1,28 +1,6 @@
 package neu.cs6650;
 
-import neu.cs6650.client.ApiClient;
-import neu.cs6650.model.ThreadInput;
-
 public class Configuration {
-//  private static ApiClient defaultApiClient = new ApiClient();
-
-  /**
-   * Get the default API client, which would be used when creating API
-   * instances without providing an API client.
-   *
-   * @return Default API client
-   */
-//  public static ApiClient getDefaultApiClient() {
-//    return defaultApiClient;
-//  }
-
-  /**
-   * Set the default API client, which would be used when creating API
-   * instances without providing an API client.
-   *
-   * @param apiClient API client
-   */
-//  public static void setDefaultApiClient(ApiClient apiClient) {
-//    defaultApiClient = apiClient;
-//  }
+  public static final boolean IS_LOCAL = true;
+  public static final boolean IS_AWS = false;
 }
