976dfb7f50783695150256da1fdbcaca5e0d3799
Tiffany Marthin <tiffanymarthin@gmail.com>
2021-05-19 00:24:29 -0700

    comment out things

--- src/main/java/neu/cs6650/client/Main.java
+++ src/main/java/neu/cs6650/client/Main.java
@@ -1,11 +1,9 @@
 package neu.cs6650.client;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
+import neu.cs6650.InputProcessor;
 
 public class Main {
 
@@ -16,16 +14,43 @@ public class Main {
     // Retrieve the results
     // Write to CSV
     // Process Statistics
-    BufferedReader bufferedReader;
-    BlockingQueue<String> blockingQueue = new ArrayBlockingQueue<>(100);
-    String inputFile ="location.txt";
+    BlockingQueue<String> blockingQueue = new ArrayBlockingQueue<>(5);
+    String inputFile = "input/test.txt";
     String eof = "-1";
-    bufferedReader = new BufferedReader(new FileReader(inputFile));
-    String line = null;
-    while ((line = bufferedReader.readLine()) != null) {
-      blockingQueue.put(line);
-    }
-    blockingQueue.put(eof);
-  }
 
+    // One thread to read the input file
+    InputProcessor inputProcessor = new InputProcessor(inputFile, blockingQueue);
+    new Thread(inputProcessor).start();
+
+    String ipAdd = "localhost";
+    String port = "8080";
+    String funct = "wordcount";
+//    MultithreadedClient client = new MultithreadedClient(3, blockingQueue, ipAdd, port, funct);
+
+//    ExecutorService executor = Executors.newFixedThreadPool(4);
+//    CompletionService<ThreadRecord> completionService = new ExecutorCompletionService<>(executor);
+//    Consumer consumer = new Consumer(blockingQueue);
+//    for (int i = 0; i < 4; i++) {
+//      completionService.submit(consumer);
+//    }
+//
+//    int totalSucc = 0, totalFail = 0;
+//    try {
+//      for (int i = 0; i < 4; i++) {
+//        Future f = completionService.take();
+//        ThreadRecord tr = (ThreadRecord) f.get();
+//        totalSucc += tr.getNSuccessRequest();
+//        totalFail += tr.getNFailedRequest();
+//      }
+//    } catch (InterruptedException e) {
+//      Thread.currentThread().interrupt();
+//    } catch (ExecutionException | CancellationException e) {
+//      System.out.println("ee or cancel e");
+//    }
+//
+//    System.out.println(totalSucc);
+//    System.out.println(totalFail);
+//    executor.shutdown();
+
+  }
 }
