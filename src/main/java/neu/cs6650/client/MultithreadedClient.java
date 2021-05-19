3b305ef2cb6d35c07ef3de9e92ef22acff535373
Tiffany Marthin <tiffanymarthin@gmail.com>
2021-05-19 00:23:37 -0700

    Implemented methods in multithreaded client: submit threads, retrieve request stats

--- src/main/java/neu/cs6650/client/MultithreadedClient.java
+++ src/main/java/neu/cs6650/client/MultithreadedClient.java
@@ -1,45 +1,68 @@
 package neu.cs6650.client;
 
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.CancellationException;
 import java.util.concurrent.CompletionService;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorCompletionService;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import neu.cs6650.api.TextbodyApi;
-import neu.cs6650.model.TextLine;
+import java.util.concurrent.Future;
 import neu.cs6650.model.ThreadInput;
 import neu.cs6650.model.ThreadRecord;
 
 public class MultithreadedClient {
+
   private Integer maxThreads;
-  private BlockingQueue<String> textInput;
+  private BlockingQueue<String> lineQueue;
 
   private String ipAddress;
   private String port;
   private String function;
 
+  private long totalSuccessfulRequests;
+  private long totalFailedRequests;
+
   private ExecutorService executor = Executors.newFixedThreadPool(maxThreads);
-  private CompletionService<ThreadRecord> completionService = new ExecutorCompletionService<>(executor);
+  private CompletionService<ThreadRecord> completionService = new ExecutorCompletionService<>(
+      executor);
 
   public MultithreadedClient(Integer maxThreads,
       BlockingQueue<String> textInput, String ipAddress, String port, String function) {
     this.maxThreads = maxThreads;
-    this.textInput = textInput;
+    this.lineQueue = textInput;
     this.ipAddress = ipAddress;
     this.port = port;
     this.function = function;
   }
 
-  private void createThreads() throws InterruptedException {
+  public void start() throws InterruptedException {
+    submitThreads();
+    updateRequestResults();
+    executor.shutdown();
+  }
+
+  private void submitThreads() {
     ThreadInput threadInput = new ThreadInput(this.ipAddress, this.port);
+    ApiClient apiClient = new ApiClient(threadInput, function, lineQueue);
     for (int i = 0; i < maxThreads; i++) {
-      TextLine body = new TextLine();
-      body.message(this.textInput.take());
-      if (!body.getMessage().equals("-1") || body.getMessage() != null) {
-        completionService.submit(new ApiClient(threadInput, body, this.function));
-      } else {
-        break;
+      completionService.submit(apiClient);
+    }
+  }
+
+  private void updateRequestResults() {
+    try {
+      for (int i = 0; i < this.maxThreads; i++) {
+        Future<ThreadRecord> f = completionService.take();
+        ThreadRecord record = f.get();
+        totalSuccessfulRequests += record.getNSuccessRequest();
+        totalFailedRequests += record.getNFailedRequest();
       }
+    } catch (InterruptedException e) {
+      Thread.currentThread().interrupt();
+    } catch (ExecutionException | CancellationException e) {
+      System.out.println("ee or cancel e");
     }
   }
 }
+
