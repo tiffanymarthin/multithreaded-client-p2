06daf24e142c7f6eb21d783eeca9c0a9c62a5a0e
Tiffany Marthin <tiffanymarthin@gmail.com>
2021-05-22 12:39:52 -0700

    Add a class to write csv report

--- src/main/java/neu/cs6650/ReportWriter.java
+++ src/main/java/neu/cs6650/ReportWriter.java
@@ -1,2 +1,34 @@
-package neu.cs6650;public class ReportWriter {
+package neu.cs6650;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.LinkedList;
+import neu.cs6650.model.LatencyRecord;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class ReportWriter {
+  private static final Logger logger = LogManager.getLogger(InputProcessor.class.getName());
+
+  private String csvOutputFile;
+  private LinkedList<LatencyRecord> latencyList;
+
+  public ReportWriter(String csvOutputFile,
+      LinkedList<LatencyRecord> latencyList) {
+    this.csvOutputFile = csvOutputFile;
+    this.latencyList = latencyList;
+  }
+
+  public void start() {
+    File outputFile = new File(this.csvOutputFile);
+    try (PrintWriter printWriter = new PrintWriter(outputFile)) {
+      printWriter.println("start time, request type, latency, response code");
+      this.latencyList.stream()
+          .map(LatencyRecord::toString)
+          .forEach(printWriter::println);
+    } catch (FileNotFoundException e) {
+      logger.info("Csv output file is not found");
+    }
+  }
 }
