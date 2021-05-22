6644c307c0bf12623877d57d9b50fd8037675457
Tiffany Marthin <tiffanymarthin@gmail.com>
2021-05-21 22:11:11 -0700

    Add latency record and list of latency record in thread record

--- src/main/java/neu/cs6650/model/ThreadRecord.java
+++ src/main/java/neu/cs6650/model/ThreadRecord.java
@@ -1,8 +1,11 @@
 package neu.cs6650.model;
 
+import java.util.List;
+
 public class ThreadRecord {
   private int nSuccessRequest;
   private int nFailedRequest;
+  private List<LatencyRecord> latencyList;
 
   public ThreadRecord(int nSuccessRequest, int nFailedRequest) {
     this.nSuccessRequest = nSuccessRequest;
@@ -15,5 +18,9 @@ public class ThreadRecord {
 
   public int getNFailedRequest() {
     return nFailedRequest;
+  }
+
+  public List<LatencyRecord> getLatencyList() {
+    return latencyList;
   }
 }
