4da8a96e531742f601a2af555ef659db291d2cc8
Tiffany Marthin <tiffanymarthin@gmail.com>
2021-05-22 12:39:14 -0700

    Add static methods in Util class to calculate aggregate stats

--- src/main/java/neu/cs6650/client/Util.java
+++ src/main/java/neu/cs6650/client/Util.java
@@ -1,5 +1,43 @@
 package neu.cs6650.client;
 
-public class Util {
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
+import java.util.stream.DoubleStream;
+import neu.cs6650.model.LatencyRecord;
+import org.apache.commons.math3.stat.descriptive.rank.Percentile;
 
+
+public class Util {
+  public static double meanResponseTime(LinkedList<LatencyRecord> latencyList) {
+    return latencyList.stream()
+        .mapToDouble(LatencyRecord::getLatency)
+        .average()
+        .orElseThrow(NoSuchElementException::new);
+  }
+
+  public static double medianResponseTime(LinkedList<LatencyRecord> latencyList) {
+    DoubleStream sortedLatencies = latencyList.stream().mapToDouble(LatencyRecord::getLatency).sorted();
+    double median = latencyList.size() % 2 == 0 ?
+        sortedLatencies.skip(latencyList.size() / 2 - 1).limit(2).average().getAsDouble() :
+        sortedLatencies.skip(latencyList.size() / 2).findFirst().getAsDouble();
+    return median;
+  }
+
+  public static double requestsPerSecond(int totalRequests, long wallTime) {
+    return totalRequests * 1000.0 / wallTime;
+  }
+
+  public static double p99ResponseTime(LinkedList<LatencyRecord> latencyList) {
+    Percentile p99 = new Percentile();
+    DoubleStream sortedLatencies = latencyList.stream().mapToDouble(LatencyRecord::getLatency).sorted();
+
+    return p99.evaluate(sortedLatencies.toArray(), 99);
+  }
+
+  public static double maxResponseTime(LinkedList<LatencyRecord> latencyList) {
+    return latencyList.stream()
+        .mapToDouble(LatencyRecord::getLatency)
+        .max()
+        .orElseThrow(NoSuchElementException::new);
+  }
 }
